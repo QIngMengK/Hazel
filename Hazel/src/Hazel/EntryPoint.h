1793a78f905379304339fb36e2157142d3cd533f
清梦 <157796080+QIngMengK@users.noreply.github.com>
2025-09-23 19:46:59 +0800

    新增日志库文件

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
 
@@ -7,7 +7,10 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc,char** argv)
 {
-
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}",a);
 	auto app = Hazel::CreateApplication();
 	app->Run();
 

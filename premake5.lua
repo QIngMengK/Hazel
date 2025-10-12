b9bdb76e09b95933d58f369afc922ef58dfc79a7
清梦 <157796080+QIngMengK@users.noreply.github.com>
2025-10-12 20:14:13 +0800

    add Glad

--- premake5.lua
+++ premake5.lua
@@ -1,130 +1,134 @@
 workspace "Hazel"
-architecture "x64"
-toolset "v143"
-configurations
-{
-"Debug",
-"Release",
-"Dist"
-}
+	architecture "x64"
+	toolset "v143"
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 
 include "Hazel/vendor/GLFW"
+include "Hazel/vendor/Glad"
 
 project "Hazel"
-location "Hazel"
-kind "SharedLib"
-language "C++"
+	location "Hazel"
+	kind "SharedLib"
+	language "C++"
 
-targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-pchheader "hzpch.h"
-pchsource "Hazel/src/hzpch.cpp"
+	pchheader "hzpch.h"
+	pchsource "Hazel/src/hzpch.cpp"
 
-files
-{
-"%{prj.name}/src/**.h",
-"%{prj.name}/src/**.cpp"
-}
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-includedirs
-{
-"%{prj.name}/src",
+	includedirs
+	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}"
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}"
 	}
 
 	links 
 	{ 
 		"GLFW",
+		"Glad",
 		"opengl32.lib"
-}
+	}
 
-filter "system:windows"
-cppdialect "C++17"
-staticruntime "off"
-systemversion "latest"
-buildoptions { "/utf-8" }
-defines
-{
-"HZ_PLATFORM_WINDOWS",
-"HZ_BUILD_DLL"
-}
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		buildoptions { "/utf-8" }
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
+		}
 
-postbuildcommands
-{
-"{MKDIR} ../bin/" .. outputdir .. "/Sandbox",
-"{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox"
-}
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
 
-filter "configurations:Debug"
-defines "HZ_DEBUG"
-buildoptions "/MDd"
-symbols "On"
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		buildoptions "/MDd"
+		symbols "On"
 
-filter "configurations:Release"
-defines "HZ_RELEASE"
-buildoptions "/MD"
-optimize "On"
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		buildoptions "/MD"
+		optimize "On"
 
-filter "configurations:Dist"
-defines "HZ_DIST"
-buildoptions "/MD"
-optimize "On"
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		buildoptions "/MD"
+		optimize "On"
 
 project "Sandbox"
-location "Sandbox"
-kind "ConsoleApp"
-language "C++"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
 
-targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-files
-{
-"%{prj.name}/src/**.h",
-"%{prj.name}/src/**.cpp"
-}
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
 
-includedirs
-{
-"Hazel/vendor/spdlog/include",
-"Hazel/src"
-}
+	includedirs
+	{
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
 
-links
-{
-"Hazel"
-}
+	links
+	{
+		"Hazel"
+	}
 
-filter "system:windows"
-cppdialect "C++17"
-staticruntime "On"
-systemversion "latest"
-buildoptions { "/utf-8" }
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		buildoptions { "/utf-8" }
 
-defines
-{
-"HZ_PLATFORM_WINDOWS"
-}
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
 
-filter "configurations:Debug"
-defines "HZ_DEBUG"
-buildoptions "/MDd"
-symbols "On"
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		buildoptions "/MDd"
+		symbols "On"
 
-filter "configurations:Release"
-defines "HZ_RELEASE"
-buildoptions "/MD"
-optimize "On"
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		buildoptions "/MD"
+		optimize "On"
 
-filter "configurations:Dist"
-defines "HZ_DIST"
-buildoptions "/MD"
-optimize "On"
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		buildoptions "/MD"
+		optimize "On"

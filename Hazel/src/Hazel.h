5211fb0730d52270dcc7769038785f4cf43d7cf7
清梦 <157796080+QIngMengK@users.noreply.github.com>
2025-10-17 19:18:23 +0800

    增加输入轮询

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -5,6 +5,10 @@
 #include "Hazel/Log.h"
 #include "Hazel/Layer.h"
 
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseButtonCodes.h"
+
 #include "Hazel/ImGui/ImGuiLayer.h"
 // ---Entry Point---------------
 #include "Hazel/EntryPoint.h"

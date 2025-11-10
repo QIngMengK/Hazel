8e0dbb2a9f7531c73cfd9c7beedf1034c867ba0b
清梦 <157796080+QIngMengK@users.noreply.github.com>
2025-11-10 16:01:23 +0800

    add Shader

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -8,7 +8,7 @@
 #include "Hazel/Events/ApplicationEvent.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
-
+#include "Hazel/Renderer/Shader.h"
 namespace Hazel {
 
 	class HAZEL_API Application
@@ -36,6 +36,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		static Application* s_Instance;
 	};
